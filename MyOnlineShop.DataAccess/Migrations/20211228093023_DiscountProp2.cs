038b7de08c07530b89e32d0ddb11d79cf1f73a31
SETAREH <setare_r2000@yahoo.com>
2021-12-28 13:07:29 +0330

    .

---
+++ MyOnlineShop.DataAccess/Migrations/20211228093023_DiscountProp2.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyOnlineShop.DataAccess.Migrations
+{
+    public partial class DiscountProp2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscountId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Discounts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Discounts_Orders_Id",
+                table: "Discounts",
+                column: "Id",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Discounts_Orders_Id",
+                table: "Discounts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DiscountId",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Discounts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
--- MyOnlineShop.Domain/Entities/Discount.cs
+++ MyOnlineShop.Domain/Entities/Discount.cs
@@ -1,13 +1,16 @@
 ﻿using MyOnlineShop.Domain.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyOnlineShop.Domain.Entities
 {
     public class Discount
     {
+        [ForeignKey("Order")]
         public int Id { get; set; }
         public string Code { get; set; }
         public DiscountType DiscountType { get; set; }
         public decimal Value { get; set; }
+        public Order Order { get; set; }
 
     }
 }
